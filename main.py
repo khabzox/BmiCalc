67111c8385fedbddb883472a6aab493df99b0890
Abdelkabir <abdelkabir.ouadoukou@gmail.com>
2024-07-20 08:22:00 +0100

    fix Deploy

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# import libs
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
 from BmiCalc import calculate_bmi
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
 
-# create app fro api
+# create app for API
 app = FastAPI()
-app.add_middleware(CORSMiddleware,
-    allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 # إعداد القوالب
@@ -26,13 +27,12 @@ async def get_form(request: Request):
 
 # Define new path
 @app.get("/calculate_bmi")
-
 def bmi_endpoint(
     weight: float = Query(..., gt=20, lt=200, description="the weight in kilograms"),
     height: float = Query(..., gt=1, lt=3, description="the height in metres")
-    ):
+):
     return calculate_bmi(weight, height)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
